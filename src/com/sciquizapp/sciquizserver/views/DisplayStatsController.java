826970bc28836a405a9fd63e686840541d3f2828
maximerchrd <maximerchrd@gmail.com>
2018-02-27 21:30:38 +0400

    worked on chart for results

--- src/com/sciquizapp/sciquizserver/views/DisplayStatsController.java
+++ src/com/sciquizapp/sciquizserver/views/DisplayStatsController.java
@@ -5,21 +5,23 @@ import com.sciquizapp.sciquizserver.database_management.DbTableIndividualQuestio
 import com.sciquizapp.sciquizserver.database_management.DbTableStudents;
 import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Vector;
@@ -30,14 +32,15 @@ import java.util.Vector;
 public class DisplayStatsController implements Initializable {
     private Vector<String> studentsVector;
 
-    private JFXPanel fxPanel;
-
     @FXML private ComboBox chart_type;
     @FXML private ComboBox time_step;
     @FXML private TreeView students_tree;
     @FXML private BarChart <String, Number> bar_chart;
+    @FXML private NumberAxis numberYAxis;
+    @FXML private CategoryAxis categoryXAxis;
+    @FXML private javafx.scene.control.ScrollPane chartScrollPane;
+    @FXML private AnchorPane anchorPane;
 
-    String usa = "";
     DisplayStatsController displayStats_singleton;
 
 
@@ -59,62 +62,44 @@ public class DisplayStatsController implements Initializable {
         //combobox with time span
         time_step.getItems().addAll("All");
         time_step.getSelectionModel().select("All");
+
+        int screenWidth = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth());
+        int screenHeight = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+        //chartScrollPane.setPrefWidth(screenWidth - time_step.getWidth() - 30);
+        //chartScrollPane.setPrefHeight(screenHeight);
+        anchorPane.setPrefWidth(screenWidth - time_step.getWidth() - 30);
+        bar_chart.setPrefWidth(screenWidth - time_step.getWidth() - 30);
     }
 
     public void displayChartButtonClicked() {
-        /*if (fxPanel == null) {
-            fxPanel = new JFXPanel();
-            displayStats_singleton.add(fxPanel);
-            initAndShowGUI(chart_entries_box.getSelectedItem().toString(), students_entries_box.getSelectedItem().toString());
-        } else {
-            displayStats_singleton.remove(fxPanel);
-            fxPanel = null;
-            fxPanel = new JFXPanel();
-            displayStats_singleton.add(fxPanel);
-            initAndShowGUI(chart_entries_box.getSelectedItem().toString(),students_entries_box.getSelectedItem().toString());
-        }*/
-        createScene(chart_type.getSelectionModel().getSelectedItem().toString(), students_tree.getSelectionModel().getSelectedItems().toArray());
+        TreeItem selectedItem = (TreeItem)students_tree.getSelectionModel().getSelectedItem();
+        drawChart(chart_type.getSelectionModel().getSelectedItem().toString(), selectedItem.getValue().toString());
     }
 
-    private void initAndShowGUI(String valuesType, Object[] students) {
-        // This method is invoked on the EDT thread
-        //fxPanel = new JFXPanel();
-        //this.add(fxPanel);
-        //DisplayStatsController displayStats = new DisplayStatsController();
-        //Scene scene = createScene(valuesType, student);
-        //fxPanel.setScene(scene);
-
-        /*Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                initFX(fxPanel);
-            }
-        });*/
+    public void eraseChartButtonClicked() {
+        bar_chart.getData().remove(0, bar_chart.getData().size());
     }
 
-    private Scene createScene(String valuesType, Object[] students) {
-        usa = String.valueOf(System.currentTimeMillis());
-        CategoryAxis xAxis = new CategoryAxis();
-        NumberAxis yAxis = new NumberAxis(0,100,10);
+    private void drawChart(String valuesType, String student) {
+
         XYChart.Series series1 = new XYChart.Series();
         if (valuesType.contentEquals("Evaluation vs subject")) {
-            xAxis.setLabel("Subjects");
-            yAxis.setLabel("Evaluation [%]");
-            Vector<Vector<String>> studentResultsPerSubject = DbTableStudents.getStudentResultsPerSubject(students[0].toString());
+            categoryXAxis.setLabel("Subjects");
+            numberYAxis.setLabel("Evaluation [%]");
+            Vector<Vector<String>> studentResultsPerSubject = DbTableStudents.getStudentResultsPerSubject(student.toString());
             Vector<String> subjects = studentResultsPerSubject.get(0);
             Vector<String> results = studentResultsPerSubject.get(1);
-            series1.setName(students[0].toString());
+            series1.setName(student.toString());
             for (int i = 0; i < subjects.size(); i++) {
                 series1.getData().add(new XYChart.Data(subjects.get(i), Double.parseDouble(results.get(i))));
             }
-            System.out.println("displaying eval vs subjects");
         } else if (valuesType.contentEquals("Evaluation vs objective")) {
-            xAxis.setLabel("Learning objectives");
-            yAxis.setLabel("Evaluation [%]");
-            Vector<Vector<String>> studentResultsPerObjective = DbTableStudents.getStudentResultsPerObjective(students[0].toString());
+            categoryXAxis.setLabel("Learning objectives");
+            numberYAxis.setLabel("Evaluation [%]");
+            Vector<Vector<String>> studentResultsPerObjective = DbTableStudents.getStudentResultsPerObjective(student.toString());
             Vector<String> objectives = studentResultsPerObjective.get(0);
             Vector<String> results = studentResultsPerObjective.get(1);
-            series1.setName(students[0].toString());
+            series1.setName(student.toString());
             for (int i = 0; i < objectives.size(); i++) {
                 if (objectives.get(i).length() > 16) {
                     objectives.set(i, objectives.get(i).substring(0,15) + "\n" + objectives.get(i).substring(15,objectives.get(i).length()));
@@ -127,20 +112,10 @@ public class DisplayStatsController implements Initializable {
                 }
                 series1.getData().add(new XYChart.Data(objectives.get(i), Double.parseDouble(results.get(i))));
             }
-            System.out.println("displaying eval vs objectives");
         }
 
-
-
-
-
-
-        int screenWidth = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth());
-        int screenHeight = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight());
-
-        Scene scene = new Scene(bar_chart, screenWidth * 0.4, screenHeight * 0.37);
         bar_chart.getData().addAll(series1);
-
-        return (scene);
+        bar_chart.setPrefWidth(chartScrollPane.getWidth());
+        bar_chart.setPrefHeight(chartScrollPane.getHeight());
     }
 }
