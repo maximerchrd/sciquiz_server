3a364e3654782cc9095fb10c31b2fa1a65013dab
maximerchrd <maximerchrd@gmail.com>
2018-03-10 16:38:19 +0400

    implemented export of results

--- src/com/sciquizapp/sciquizserver/views/ResultsTableController.java
+++ src/com/sciquizapp/sciquizserver/views/ResultsTableController.java
@@ -10,6 +10,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -18,6 +21,7 @@ import java.util.ResourceBundle;
  * Created by maximerichard on 01.03.18.
  */
 public class ResultsTableController implements Initializable {
+
     @FXML private TableView<SingleResultForTable> resultsTable;
     @FXML private TableColumn<SingleResultForTable, String> Name;
     @FXML private TableColumn<SingleResultForTable, String> Date;
@@ -44,11 +48,36 @@ public class ResultsTableController implements Initializable {
         addResults();
     }
 
-    void addResults() {
+    private void addResults() {
         ObservableList<SingleResultForTable> data = resultsTable.getItems();
         ArrayList<SingleResultForTable> resultsList = DbTableIndividualQuestionForStudentResult.getAllSingleResults();
         for (int i = 0; i < resultsList.size(); i++) {
             data.add(resultsList.get(i));
         }
+    }
+
+    public void exportResults() {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("results.csv", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        writer.println("Name;Date;Question;Evaluation;Student's answer;Correct answer;Incorrect answer;Subjects;Objectives");
+        ArrayList<SingleResultForTable> resultsList = DbTableIndividualQuestionForStudentResult.getAllSingleResults();
+        for (int i = 0; i < resultsList.size(); i++) {
+            writer.print(resultsList.get(i).getName() + ";");
+            writer.print(resultsList.get(i).getDate() + ";");
+            writer.print(resultsList.get(i).getQuestion().replace(";",",") + ";");
+            writer.print(resultsList.get(i).getEvaluation() + ";");
+            writer.print(resultsList.get(i).getStudentsAnswer().replace(";",",") + ";");
+            writer.print(resultsList.get(i).getCorrectAnswer().replace(";",",") + ";");
+            writer.print(resultsList.get(i).getIncorrectAnswer().replace(";",",") + ";");
+            writer.print(resultsList.get(i).getSubjects().replace(";",",") + ";");
+            writer.print(resultsList.get(i).getObjectives().replace(";",",") + "\n");
+        }
+        writer.close();
     }
 }
