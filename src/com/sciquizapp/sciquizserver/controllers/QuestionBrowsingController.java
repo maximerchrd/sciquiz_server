4c90116cd14cf29168ebd2c4cde423ba329ff493
maximerchrd <maximerchrd@gmail.com>
2018-03-13 16:31:19 +0400

    ip label restored

--- src/com/sciquizapp/sciquizserver/controllers/QuestionBrowsingController.java
+++ src/com/sciquizapp/sciquizserver/controllers/QuestionBrowsingController.java
@@ -1,8 +1,14 @@
 package com.sciquizapp.sciquizserver.controllers;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ResourceBundle;
 
 /**
@@ -10,7 +16,31 @@ import java.util.ResourceBundle;
  */
 public class QuestionBrowsingController implements Initializable {
 
+    @FXML private Label labelIP;
+
     public void initialize(URL location, ResourceBundle resources) {
-        
+        final String[] ip_address = {""};
+        Task<Void> getIPTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                ip_address[0] = InetAddress.getLocalHost().getHostAddress();
+                Platform.runLater(() -> labelIP.setText("students should connect \nto the following address: " + ip_address[0]));
+                return null;
+            }
+        };
+        new Thread(getIPTask).start();
+    }
+
+    public void refreshIP() {
+        final String[] ip_address = {""};
+        Task<Void> getIPTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                ip_address[0] = InetAddress.getLocalHost().getHostAddress();
+                Platform.runLater(() -> labelIP.setText("students should connect \nto the following address: " + ip_address[0]));
+                return null;
+            }
+        };
+        new Thread(getIPTask).start();
     }
 }
